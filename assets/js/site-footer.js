e9209069c33e7b7da6ac90e25436d8dc66502f64
Jason Feimster <jasonfeimster@gmail.com>
2026-07-03 13:59:06 -0400

    Add footer utility module placeholder

---
+++ assets/js/site-footer.js
@@ -0,0 +1,12 @@
+/*
+ * Footer utility namespace for future footer-specific enhancements.
+ * Rendering is handled by assets/js/site-nav.js to keep nav/footer data in one public-safe fetch.
+ */
+(function () {
+  "use strict";
+  window.AmIFundableFooter = {
+    version: "1.0.0",
+    visibility: "public_browser_safe",
+    renderedBy: "assets/js/site-nav.js"
+  };
+})();
